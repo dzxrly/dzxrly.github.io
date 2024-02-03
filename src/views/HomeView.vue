28e6f92dee48c19aeb8377467ac8862cdb0825f6
dzxrly <dzxrly2@gmail.com>
2024-02-03 16:45:37 +0800

    :tada: 重新开始个人主页项目，旧项目移动至 ./archived

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home-view-wrapper"></div>
 </template>
