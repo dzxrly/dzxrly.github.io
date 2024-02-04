f5b710e83ed16f000891f36a9c9882f0350c07cb
dzxrly <dzxrly2@gmail.com>
2024-02-04 13:05:49 +0800

    :construction_worker: 构建测试

--- docs/assets/use-quasar.0188df6d.js
+++
@@ -1,7 +0,0 @@
-import { a3 as r, i as a } from './index.db71f415.js'
-
-function u() {
-  return a(r)
-}
-
-export { u }
---
+++ docs/assets/use-quasar.38522ee3.js
@@ -0,0 +1,7 @@
+import { a3 as r, i as a } from './index.4f764e17.js'
+
+function u() {
+  return a(r)
+}
+
+export { u }
