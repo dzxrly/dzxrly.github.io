cf0f221d93737452ac54d30a2603a02b7db77d42
EggTargaryen <dzxrly2@gmail.com>
2024-06-15 16:02:57 +0800

    :art: 修复了"anyscript"的问题

---
+++ docs/assets/use-quasar.2b2a48f2.js
@@ -0,0 +1,1 @@
+import{i as a,a6 as r}from"./index.7d66cf72.js";function u(){return a(r)}export{u};
---
+++ docs/assets/use-timeout.94726bb5.js
@@ -0,0 +1,1 @@
+import{b as u}from"./QBtn.497a1d04.js";import{B as i,t as m,g as s}from"./index.7d66cf72.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
