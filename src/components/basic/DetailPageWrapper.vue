342deff53072973021ab09c051074b0f610551e4
EggTargaryen <dzxrly2@gmail.com>
2024-07-12 14:53:07 +0800

    :lipstick: MainCard换用grid布局

--- src/components/basic/DetailPageWrapper.vue
+++ src/components/basic/DetailPageWrapper.vue
@@ -8,7 +8,7 @@ const isLtSm = computed(() => $q.screen.lt.sm);
 </script>
 
 <template>
-  <div class="detail-page-wrapper full-width full-height">
+  <div class="detail-page-wrapper full-width full-height q-px-md">
     <div class="back-btn-row row justify-start items-center full-width q-mb-md">
       <q-btn
         :size="isLtSm ? 'md' : 'lg'"
