5cd750477125e5f7ad26c341ca353a1999094ac0
EggTargaryen <dzxrly2@gmail.com>
2024-08-29 10:42:44 +0800

    :art: 优化了字体加载过程

--- docs/assets/use-quasar.36961f8a.js
+++ docs/assets/use-quasar.36961f8a.js
@@ -1,5 +1,1 @@
-import { i as a, a6 as r } from './index.59749b07.js';
-function u() {
-  return a(r);
-}
-export { u };
+import{i as a,a6 as r}from"./index.59749b07.js";function u(){return a(r)}export{u};
