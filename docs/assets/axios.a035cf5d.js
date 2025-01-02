cbf7f5aaa6f2ac45f2d288243e4acae015d29b60
Egg Targaryen <dzxrly2@gmail.com>
2025-01-02 10:41:16 +0800

    :art: 优化了代码和UI

--- docs/assets/axios.a035cf5d.js
+++ docs/assets/axios.a035cf5d.js
@@ -1,1 +1,7 @@
-import{b as i}from"./index.9c1a1e44.js";import{a}from"./axios.6b484fa5.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
+import { b as i } from './index.9c1a1e44.js';
+import { a } from './axios.6b484fa5.js';
+const e = a.create({ baseURL: 'https://api.example.com' });
+var t = i(({ app: o }) => {
+  (o.config.globalProperties.$axios = a), (o.config.globalProperties.$api = e);
+});
+export { e as api, t as default };
--- docs/assets/use-quasar.7c54f452.js
+++ docs/assets/use-quasar.7c54f452.js
@@ -1,1 +1,5 @@
-import{i as a,a6 as r}from"./index.9c1a1e44.js";function u(){return a(r)}export{u};
+import { i as a, a6 as r } from './index.9c1a1e44.js';
+function u() {
+  return a(r);
+}
+export { u };
